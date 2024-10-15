2f4d920e914d3cc4a278b1b4ecc7dc1a5453a17f
Pedro Henrique <105245403+pedrohgreis@users.noreply.github.com>
2024-10-14 20:15:38 -0400

    Uploading project with some tests done

--- Medico.ts
+++ Medico.ts
@@ -1,10 +1,12 @@
 export class Medico{
     private _nome:string;
     private _credencial: string;
+	private _especialidade: string;
 
-	constructor(nome: string, credencial: string) {
+	constructor(nome: string, credencial: string, especialidade: string) {
 		this._nome = nome;
-		this._credencial = credencial;
+		this.credencial = credencial;
+		this._especialidade = especialidade;
 	}
 
 	public get nome(): string {
@@ -15,6 +17,10 @@ export class Medico{
 		return this._credencial;
 	}
 
+	public get especialidade():string{
+		return this._especialidade;
+	}
+
 	public set nome(nome: string){
 		this._nome = nome;
 	}
@@ -22,6 +28,10 @@ export class Medico{
 	public set credencial(credencial: string) {
 		if(!isNaN(parseFloat(credencial)) && (credencial.length) === 6)
 			this._credencial = credencial;
+	}
+
+	public set especialidade(especialidade:string){
+		this._especialidade = especialidade; 
 	}
 
 }
--- Paciente.ts
+++ Paciente.ts
@@ -1,13 +1,23 @@
 export class Paciente{
     private _nome:string;
+    private _idade:number
 
-    constructor(nome:string){
+    constructor(nome:string, idade: number){
         this._nome = nome;
+        this._idade = idade;
     }
     get nome(){
         return this._nome;
     }
     set nome(nome:string){
         this._nome = nome;
+    }
+
+    get idade(){
+        return this._idade;
+    }
+
+    set idade(idade:number){
+        this._idade = idade;
     }
 }
