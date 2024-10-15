4fd7c541da4f9cc75e7999222e55b8aa8f2898dc
Thiago Vinicius <tbrandao2017@hotmail.com>
2024-10-15 15:32:34 -0400

    Teste Receita
    Teste Receita

--- Receita.test.ts
+++ Receita.test.ts
@@ -5,8 +5,15 @@ import { Paciente } from "./Paciente";
 
 describe('paciente', () =>{
 
+    let paciente:Paciente
+    let consulta: Consulta
+    beforeEach(() => {
+    paciente = new Paciente("Luana",24);
+    consulta = new Consulta(new Medico("Joao","123456","Cardiologista"),new Date('2024-11-05'), paciente)
+})
+
     test("Verificando mediciamento", () => {
-        const receita1 = new Receita("Dipirona",new Consulta(new Medico("Joao","123456","Cardiologista"),new Date("2024-11-05"),new Paciente("Lucas",25)));
+        const receita1 = new Receita("Dipirona", consulta);
         expect(receita1.medicamentos).toBe("Dipirona");
     })
 
