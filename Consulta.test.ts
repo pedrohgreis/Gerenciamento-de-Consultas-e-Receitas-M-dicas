effe616c782f41d4442cc3dc5c61d5a969466823
Pedro Henrique <105245403+pedrohgreis@users.noreply.github.com>
2024-10-16 19:08:55 -0400

    Mocking test Consulta

--- Consulta.test.ts
+++ Consulta.test.ts
@@ -4,16 +4,27 @@ import { Consulta } from "./Consulta";
 
 let medico:Medico
 let paciente:Paciente
+
+beforeAll(() => {
+    // Mockando a data atual para 2024-10-09
+    jest.useFakeTimers().setSystemTime(new Date('2024-10-09').getTime());
+  });
+
 beforeEach(() => {
     medico = new Medico("Cristiano Ronaldo","123456","Cardiologista");
     paciente = new Paciente("Luana",24);
-})
+});
+
+afterAll(() => {
+    // Restaurando timers reais para não interferir em outros testes
+    jest.useRealTimers();
+  });
 
 describe('Consulta', () => {
-   test('Verificando data atual', () => {
-    const consulta = new Consulta(medico,new Date('2024-10-10'),paciente)
-    expect(consulta.dataC.getTime()).toBe(new Date('2024-10-10').getTime());
-   })
+    test('Verificando data atual', () => {
+        const consulta = new Consulta(medico, new Date('2024-10-10'), paciente);
+        expect(consulta.dataC.getTime()).toBe(new Date('2024-10-10').getTime());
+    });
 
    test("Verificando data menor que data atual", () => {
     const consulta = new Consulta(medico, new Date('2024-10-10'), paciente);
