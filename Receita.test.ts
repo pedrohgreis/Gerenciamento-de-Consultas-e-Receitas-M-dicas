4df8c387d17715de9fb7c199551c9f760074ca68
Thiago Vinicius <tbrandao2017@hotmail.com>
2024-10-16 20:11:55 -0400

    Mock Receita
    Mock Receita

--- Receita.test.ts
+++ Receita.test.ts
@@ -9,6 +9,12 @@ describe('receita', () =>{
     let consulta: Consulta
     let medico: Medico
 
+    beforeAll(() => {
+        // Mockando a data atual para 2024-10-09
+        jest.useFakeTimers().setSystemTime(new Date('2024-10-09').getTime());
+      });
+    
+
     beforeEach(() => {
 
     paciente = new Paciente("Luana",24);
@@ -16,6 +22,11 @@ describe('receita', () =>{
     consulta = new Consulta(medico, new Date('2024-11-05'), paciente)
 
 })
+
+    afterAll(() => {
+        // Restaurando timers reais para não interferir em outros testes
+        jest.useRealTimers();
+    });
 
     test("Verificando mediciamento", () => {
         const receita1 = new Receita("Dipirona", consulta, new Date('2024-11-05'));
